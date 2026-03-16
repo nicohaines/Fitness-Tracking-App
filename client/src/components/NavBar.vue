22f667fa09aed237a3b47a9d0942dd5333e0aba6
nicohaines11 <nicohaines11@gmail.com>
2026-03-16 14:08:25 -0400

    Account display for activity, statistics, & friends

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -18,9 +18,9 @@ function handleLogout() {
   <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
     <div class="container">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
+        <RouterLink to="/" active-class="is-active" class="navbar-item">
           <img alt="Vue logo" src="@/assets/logo.svg" width="30" height="30" />
-        </a>
+        </RouterLink>
 
         <a
           role="button"
--- client/src/views/FriendsView.vue
+++ client/src/views/FriendsView.vue
@@ -1,6 +1,36 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useUserStore } from '../stores/users'
+
+const userStore = useUserStore()
+
+const currentUser = computed(() => userStore.currentUser)
+
+const friendUsers = computed(() => {
+  if (!currentUser.value) return []
+  return userStore.users.filter((user) => currentUser.value!.friends.includes(user.id))
+})
+</script>
+
 <template>
-  <main>
+  <main class="section">
     <h1 class="title is-2">Friends</h1>
-    <p>Placeholder: friends list and friend profile statistics are shown here.</p>
+
+    <div v-if="currentUser">
+      <h2 class="title is-4">{{ currentUser.displayName }}</h2>
+
+      <div v-if="friendUsers.length" class="content">
+        <ul>
+          <li v-for="friend in friendUsers" :key="friend.id">
+            {{ friend.displayName }} (@{{ friend.username }})
+          </li>
+        </ul>
+      </div>
+      <p v-else>No friends listed for this user.</p>
+    </div>
+
+    <div v-else class="notification is-warning is-light">
+      Please log in to view friends.
+    </div>
   </main>
 </template>
